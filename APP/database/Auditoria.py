902929f9add0a2747e600586809f4e84f576bef7
Juanj820 <jjramirez820@gmail.com>
2025-05-20 15:20:50 -0500

    Actualizacion base de datos

--- APP/database/Auditoria.py
+++
@@ -1,6 +0,0 @@
-From datatime import datetime
-
-def registrar_accion(usuario_id, accion, descripcion):
-    # Aqui podrias guardar en un archivo o tabla de auditoria
-    with open("auditoria.log", "a") as f:
-        f.write(f"{datetime.now()} - Usuario {usuario_id} - {accion}: {descripcion}\n")
