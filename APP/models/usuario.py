57389ff22bb96dbf977312015cfa6ae85a82598d
Juanj820 <jjramirez820@gmail.com>
2025-05-20 11:17:16 -0500

    Actualizacion carpeta models

---
+++ APP/models/usuario.py
@@ -0,0 +1,9 @@
+class Usuario:
+    def __init__(self, id, nombre, usuario, password, rol, creado_en, actualizado_en):
+        self.id = id
+        self.nombre = nombre
+        self.usuario = usuario
+        self.password = password
+        self.rol = rol
+        self.creado_en = creado_en
+        self.actualizado_en = actualizado_en
